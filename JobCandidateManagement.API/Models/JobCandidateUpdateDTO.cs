b312532629b08cd11a4e9ad6a3e6d451120ef919
John Rugina <johnrugina@yahoo.co.in>
2022-11-09 19:02:05 +0300

    Add models

---
+++ JobCandidateManagement.API/Models/JobCandidateUpdateDTO.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateManagement.Models
+{
+    public class JobCandidateUpdateDTO
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            ErrorMessage = "Invalid Email Format")]
+        public string EmailAddress { get; set; } = string.Empty;
+
+        public TimeSpan TimeInterval { get; set; }
+        public string LinkedInUrl { get; set; } = string.Empty;
+        public string GitHubUrl { get; set; } = string.Empty;
+        [Required]
+        public string Comments { get; set; } = string.Empty;
+    }
+}

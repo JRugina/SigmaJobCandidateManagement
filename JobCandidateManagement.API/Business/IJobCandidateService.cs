16ba0a701d193521ee3c0033d25ab4f539debbdc
John Rugina <johnrugina@yahoo.co.in>
2022-11-09 18:57:00 +0300

    Add business logic layer

---
+++ JobCandidateManagement.API/Business/IJobCandidateService.cs
@@ -0,0 +1,11 @@
+﻿using JobCandidateManagement.DataAccess.Entities;
+
+namespace JobCandidateManagement.Business
+{
+    public interface IJobCandidateService
+    {
+        void AddJobCandidate(JobCandidate jobCandidate);
+        void UpdateJobCandidate(JobCandidate jobCandidate);
+        JobCandidate? GetJobCandidate(string emailAddress);
+    }
+}
---
+++ JobCandidateManagement.API/Business/JobCandidateCSVService.cs
@@ -0,0 +1,28 @@
+﻿using JobCandidateManagement.DataAccess.Entities;
+using JobCandidateManagement.DataAccess.Services;
+
+namespace JobCandidateManagement.Business
+{
+    public class JobCandidateCSVService:IJobCandidateService
+    {
+        private readonly IJobCandidateRepository _repository;
+
+        public JobCandidateCSVService(IJobCandidateRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public void AddJobCandidate(JobCandidate jobCandidate)
+        {
+            _repository.AddJobCandidate(jobCandidate);
+        }
+        public void UpdateJobCandidate(JobCandidate jobCandidate)
+        {
+            _repository.UpdateJobCandidate(jobCandidate);
+        }
+        public JobCandidate? GetJobCandidate(string emailAddress)
+        {
+            return  _repository.GetJobCandidate(emailAddress);
+        }
+    }
+}

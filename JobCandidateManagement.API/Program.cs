0afe7d0e3ac58bbef1d4c3f04ce932fce0543690
John Rugina <johnrugina@yahoo.co.in>
2022-11-09 18:59:25 +0300

    Add configuration in dependency injection container

--- JobCandidateManagement.API/Program.cs
+++ JobCandidateManagement.API/Program.cs
@@ -1,3 +1,6 @@
+using JobCandidateManagement.Business;
+using JobCandidateManagement.DataAccess.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IJobCandidateService, JobCandidateCSVService>();
+builder.Services.AddScoped<IJobCandidateRepository, JobCandidateCSVRepository>();
 
 var app = builder.Build();
 

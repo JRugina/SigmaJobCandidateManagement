0e59e11cd3a0514fa7cd77ae8dae0426ab2b016b
John Rugina <johnrugina@yahoo.co.in>
2022-11-09 19:55:11 +0300

    Add ShouldUpdateCandidateAndReturnOkResult test case

--- JobCandidateManagement.Test/JobCandidateControllerTest.cs
+++ JobCandidateManagement.Test/JobCandidateControllerTest.cs
@@ -5,26 +5,26 @@ using Moq;
 using JobCandidateManagement.DataAccess.Services;
 using JobCandidateManagement.API.Controllers;
 using JobCandidateManagement.Business;
+using System.Net;
 
 namespace JobCandidateManagement.Test
 {
     public class JobCandidateControllerTest
     {
         private readonly JobCandidateController _jobCandidateController;
+        private readonly JobCandidateCSVService _jobCandidateService;
 
         public JobCandidateControllerTest()
         {
-            var jobCandidateCSVRepository = new JobCandidateCSVRepository();
-            var jobCandidateService = new JobCandidateCSVService(jobCandidateCSVRepository);
+            var _jobCandidateCSVRepository = new JobCandidateCSVRepository();
+            _jobCandidateService = new JobCandidateCSVService(_jobCandidateCSVRepository);
 
             var mapperConfiguration = new MapperConfiguration(
               cfg => cfg.AddProfile<JobCandidateManagement.Profiles.JobCandidateProfile>());
             var mapper = new Mapper(mapperConfiguration);
 
-            _jobCandidateController = new JobCandidateController(jobCandidateService, mapper);
+            _jobCandidateController = new JobCandidateController(_jobCandidateService, mapper);
         }
-
-
 
         [Fact]
         public void ShouldCreateANewCandidateAndReturnOkResult()
@@ -35,7 +35,7 @@ namespace JobCandidateManagement.Test
                 FirstName = "Mike",
                 LastName = "Jones",
                 PhoneNumber = "100-202-123",
-                EmailAddress = "mikejones.xyz.com",
+                EmailAddress = "mikejones@xyz.com",
                 TimeInterval = new TimeSpan(2, 14, 18),
                 LinkedInUrl = "https://www.linkedin.com/in/mike-jones",
                 GitHubUrl = "https://www.github.com/MJones",
@@ -43,10 +43,35 @@ namespace JobCandidateManagement.Test
             };
 
             //Act
-            var data = _jobCandidateController.UpdateJobCandidate(_jobCandidateDTO.EmailAddress, _jobCandidateDTO);
+            var response = _jobCandidateController.UpdateJobCandidate(_jobCandidateDTO.EmailAddress, _jobCandidateDTO);
 
             //Assert
-            Assert.IsType<OkObjectResult>(data);
+            Assert.IsType<OkResult>(response);
         }
+
+        [Fact]
+        public void ShouldUpdateCandidateAndReturnOkResult()
+        {
+            //Arrange
+            var _jobCandidateDTO = new JobCandidateUpdateDTO()
+            {
+                FirstName = "Mike",
+                LastName = "Johnson",
+                PhoneNumber = "100-202-123",
+                EmailAddress = "mikejones@xyz.com",
+                TimeInterval = new TimeSpan(2, 14, 18),
+                LinkedInUrl = "https://www.linkedin.com/in/mike-johnson",
+                GitHubUrl = "https://www.github.com/MJohnson",
+                Comments = "This is a new applicant."
+            };
+
+            //Act
+            var data = _jobCandidateController.UpdateJobCandidate(_jobCandidateDTO.EmailAddress, _jobCandidateDTO);
+
+            //Assert  
+            Assert.IsType<OkResult>(data);
+        }
+
+
     }
 }

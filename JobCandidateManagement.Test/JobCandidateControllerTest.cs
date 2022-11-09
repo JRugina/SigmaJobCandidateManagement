2a9c8fe668fde102b75dfccacd2e8a4e5de68004
John Rugina <johnrugina@yahoo.co.in>
2022-11-09 19:39:04 +0300

    Add ShouldCreateANewCandidateAndReturnOkResult test case.

--- JobCandidateManagement.Test/JobCandidateControllerTest.cs
+++ JobCandidateManagement.Test/JobCandidateControllerTest.cs
@@ -23,5 +23,30 @@ namespace JobCandidateManagement.Test
 
             _jobCandidateController = new JobCandidateController(jobCandidateService, mapper);
         }
+
+
+
+        [Fact]
+        public void ShouldCreateANewCandidateAndReturnOkResult()
+        {
+            //Arrange
+            var _jobCandidateDTO = new JobCandidateUpdateDTO()
+            {
+                FirstName = "Mike",
+                LastName = "Jones",
+                PhoneNumber = "100-202-123",
+                EmailAddress = "mikejones.xyz.com",
+                TimeInterval = new TimeSpan(2, 14, 18),
+                LinkedInUrl = "https://www.linkedin.com/in/mike-jones",
+                GitHubUrl = "https://www.github.com/MJones",
+                Comments = "This candidate had applied for a job before."
+            };
+
+            //Act
+            var data = _jobCandidateController.UpdateJobCandidate(_jobCandidateDTO.EmailAddress, _jobCandidateDTO);
+
+            //Assert
+            Assert.IsType<OkObjectResult>(data);
+        }
     }
 }
